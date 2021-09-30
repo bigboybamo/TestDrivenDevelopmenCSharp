3cb56b8987472da1b8a237faa1f37f6154425e6c
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2021-09-30 11:02:33 +0100

    first commit

---
+++ RoomBookingApp.Core.test/RoomBookingrequestprocessortest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+
+namespace RoomBookingApp.Core
+{
+    public class RoomBookingrequestprocessortest
+    {
+        [Fact]
+        public void Should_Return_Room_Booking_Response_with_Request_values()
+        {
+            //Arrange portion
+            var Request = new RoomBookingrequest
+            {
+                FullName = "Test name",
+                Email = "test@test.com",
+                Date = new DateTime(2021, 09, 29)
+            };
+
+            var processor = new RoomBookingProcessor();
+
+            //Act portion
+            RoomBookingResult result = processor.BookRoom(Request);
+
+            //Assert
+            //Assert.NotNull(result);
+            //Assert.Equal(Request.FullName, result.FullName);
+            //Assert.Equal(Request.Email, result.Email);
+            //Assert.Equal(Request.Date, result.Date);
+
+            result.ShouldNotBeNull();
+            result.FullName.ShouldBe(Request.FullName);
+            result.Email.ShouldBe(Request.Email);
+            result.Date.ShouldBe(Request.Date);
+
+
+        }
+    }
+}
---
+++ RoomBookingApp.Core/Processors/RoomBookingProcessor.cs
@@ -0,0 +1,24 @@
+﻿using RoomBookingApp.Core.Models;
+using System;
+
+namespace RoomBookingApp.Core.Processors
+{
+    public class RoomBookingProcessor
+    {
+        public RoomBookingProcessor()
+        {
+
+        }
+
+        public RoomBookingResult BookRoom(RoomBookingrequest bookingrequest)
+        {
+            return new RoomBookingResult
+            {
+                FullName = bookingrequest.FullName,
+                Email = bookingrequest.Email,
+                Date = bookingrequest.Date
+
+            };
+        }
+    }
+}
